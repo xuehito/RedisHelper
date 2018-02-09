d16df2a8fff655166be9fab7e5b971c7c3fbc526
xuehito <msxuehaitao@hotmail.com>
2018-02-09 13:40:43 +0800

    添加项目文件。

---
+++ RedisHelper/IRedisProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisHelper
+{
+    public interface IRedisProvider
+    {
+        string Get(string key);
+        string Get<T>(string key);
+        void Set(string key, string value);
+        void Set<T>(string key, T value);
+        void Set(string key, string value, TimeSpan sp);
+        void Set<T>(string key, T value, TimeSpan sp);
+        void Remove(string key);
+        List<string> SearchKeys(string key);
+        bool KeyExists(string key);
+    }
+}
---
+++ RedisHelper/RedisProvider.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisHelper
+{
+    public class RedisProvider : IRedisProvider
+    {
+        public string Get(string key)
+        {
+            try
+            {
+                return RedisHelper.GetObjectKey(key);
+            }
+            catch (Exception ex)
+            {
+                //LOG
+                return null;
+            }
+        }
+
+        public string Get<T>(string key)
+        {
+            try
+            {
+                return RedisHelper.GetObjectKey<T>(key);
+            }
+            catch (Exception ex)
+            {
+                //LOG
+                return null;
+            }
+        }
+
+        public void Set(string key, string value)
+        {
+            try
+            {
+                RedisHelper.SetStringKey(key, value);
+            }
+            catch (Exception ex)
+            {
+                //LOG
+            }
+        }
+
+        public void Set<T>(string key, T value)
+        {
+            try
+            {
+                RedisHelper.SetStringKey<T>(key, value);
+            }
+            catch (Exception ex)
+            {
+                //LOG
+            }
+        }
+
+        public void Set(string key, string value, TimeSpan sp)
+        {
+            try
+            {
+                RedisHelper.SetStringKey(key, value, sp);
+            }
+            catch (Exception ex)
+            {
+                //LOG
+            }
+        }
+
+        public void Set<T>(string key, T value, TimeSpan sp)
+        {
+            try
+            {
+                RedisHelper.SetStringKey<T>(key, value, sp);
+            }
+            catch (Exception ex)
+            {
+                //LOG
+            }
+        }
+
+        public void Remove(string key)
+        {
+            try
+            {
+                RedisHelper.KeyDelete(key);
+            }
+            catch (Exception ex)
+            {
+                //LOG
+            }
+        }
+
+        public List<string> SearchKeys(string key)
+        {
+            try
+            {
+                return RedisHelper.SearchKeys(key);
+            }
+            catch (Exception ex)
+            {
+                //LOG
+                return null;
+            }
+        }
+
+        public bool KeyExists(string key)
+        {
+            try
+            {
+                return RedisHelper.KeyExists(key);
+            }
+            catch (Exception ex)
+            {
+                //LOG
+                return false;
+            }
+        }
+    }
+}
